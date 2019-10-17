86c105ec7dc15d709464a187059c0d918d2d1e35
Dmitriymush <powder322@gmail.com>
2019-10-17 16:58:16 +0300

    solution

---
+++ dom basics.js
@@ -0,0 +1,43 @@
+'use strict';
+
+function list(data) {
+  let ul = document.createElement('ul');
+
+  for ( const dat in data ) {
+    let li = document.createElement('li');
+    li.textContent = dat;
+
+    if (data[dat]) {
+       li.append(list(data[dat]));
+    }
+
+    ul.append(li);
+  }
+
+  return ul;
+}
+
+function createTree (container, data) {
+  container.append(list(data));
+}
+
+let container = document.getElementById('container');
+let data = {
+  "fish": {
+    "trout": {},
+    "salmon": {}
+  },
+
+  "trees": {
+    "huge": {
+      "sequoia": {},
+      "oak": {}
+    },
+    "flower-tree": {
+      "apple-tree": {},
+      "magnolia": {}
+    }
+  }
+};
+
+createTree(container, data);
